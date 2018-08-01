1cee115075b207e4aefa7d689f73bc5c5d9f14a9
rory7 <aannnaa@foxmail.com>
2018-08-01 13:01:26 +0800

    数据库 外网配置 房源跟进功能 信息检索功能

--- application/house/controller/AdminEsf.php
+++ application/house/controller/AdminEsf.php
@@ -66,7 +66,8 @@ class AdminEsf extends AdminBase
     }
 
 
-    public function edit($id){
+    public function edit($id)
+    {
         global $_W, $_GPC;
         $model = set_model($this->house_esf);
         /** @var Models $model_info */
@@ -101,7 +102,8 @@ class AdminEsf extends AdminBase
     }
 
 
-    public function delete($id){
+    public function delete($id)
+    {
         global $_W, $_GPC;
         $model = set_model($this->house_esf);
         $model_info = $model->model_info;
@@ -109,10 +111,45 @@ class AdminEsf extends AdminBase
         $where['site_id'] = $_W['site']['id'];
         $detail = $model->where($where)->find();
 
-        if($detail){
-            $model_info::delete_item($id ,$this->house_esf );
+        if ($detail) {
+            $model_info::delete_item($id, $this->house_esf);
         }
 
-        return ['code' => 1 , 'msg' => 'ok'];
+        return ['code' => 1, 'msg' => 'ok'];
+    }
+
+    public function record($id)
+    {
+        global $_W, $_GPC;
+        $model = set_model($this->house_esf);
+        /** @var Models $model_info */
+        $model_info = $model->model_info;
+        $where['id'] = $id;
+        $where['site_id'] = $_W['site']['id'];
+        $detail = $model->where($where)->find();
+
+        if ($this->isPost()) {
+            if (isset($_GPC['_form_manual'])) {
+                //手动处理数据
+                $base_info = $_GPC;
+            } else {
+                //自动获取data分组数据
+                $base_info = input('post.data/a');//get the base info
+            }
+
+
+            $res = $model_info->edit_content($base_info, $where);
+            if ($res['code'] == 1) {
+                return $this->zbn_msg($res['msg'], 1, 'true', 1000, "''", "'reload_page()'");
+            } else {
+                return $this->zbn_msg($res['msg'], 2);
+            }
+
+        } else {
+            $this->view->field_list = $model_info->get_admin_publish_fields($detail, []);
+            $detail['data'] = mhcms_json_decode($detail['data']);
+            $this->view->detail = $detail;
+            return $this->view->fetch();
+        }
     }
 }
--- config/database.php
+++ config/database.php
@@ -12,13 +12,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '47.94.83.227',
     // 数据库名
     'database'        => 'fangchan',
     // 用户名
-    'username'        => 'fangchan',
+    'username'        => 'root',
     // 密码
-    'password'        => 'sf19841121',
+    'password'        => 'seven7fly',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
