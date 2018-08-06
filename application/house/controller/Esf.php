16df0394ca1f6c63357e585ed1d22af8612d73e8
rory7 <aannnaa@foxmail.com>
2018-08-06 18:37:34 +0800

    二手房修改-缺少支付查看 完整逻辑

--- application/house/controller/Esf.php
+++ application/house/controller/Esf.php
@@ -42,13 +42,29 @@ class Esf extends HouseBase
         /** @var Models $model_info */
         $model_info = $model->model_info;
 
-        $detail = Models::get_item($id, $content_model_id);		//		$detail['mobile'] = '*********';//		$is_phone = Db::table('buy_phone')->where('user_id' , )->find();		
+        $detail = Models::get_item($id, $content_model_id);        //		$detail['mobile'] = '*********';//		$is_phone = Db::table('buy_phone')->where('user_id' , )->find();
         $this->view->detail = $detail;
         $this->view->page_title = $detail['title'];
-        Hits::hit($id ,$this->house_esf);
+        Hits::hit($id, $this->house_esf);
         $this->mapping = array_merge($this->mapping, $detail);
         $this->view->seo = $this->seo($this->mapping);
-        $this->view->user_verify = set_model("users_verify")->where(['user_id'=>$detail['user_id']])->find();
+        $this->view->user_verify = set_model("users_verify")->where(['user_id' => $detail['user_id']])->find();
+
+        //设置可见权限：支付查看信息
+        $user_role_id = $this->user['user_role_id'];
+        $show_power = false;
+        if ($user_role_id == 2 || $user_role_id == 4 || $user_role_id == 5) {
+            $show_power = false;
+        } else if ($user_role_id == 1 || $user_role_id == 3 || $user_role_id == 22 || $user_role_id == 33) {
+            $show_power = true;
+        }else{
+            $show_power = false;
+        }
+        $this->assign("show_power", $show_power);
+
+        //设置支付查看交易结果
+        $pay_result = false;
+        $this->assign("pay_result", $pay_result);
         return $this->view->fetch();
     }
 }
