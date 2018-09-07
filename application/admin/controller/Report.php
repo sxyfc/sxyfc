e57a8eb0d1a00d169a6687dfbb10bc4834ced556
flank.fan <1060142557@qq.com>
2018-09-07 13:45:26 +0800

    '报表下载'

--- application/admin/controller/Report.php
+++ application/admin/controller/Report.php
@@ -16,9 +16,23 @@ class Report extends AdminBase
     public function share_profit()
     {
         $data = array();
+        $nickname = trim(input('param.nickname', ' ', 'htmlspecialchars'));
+        if ($nickname) {
+            $where['nickname'] = array('LIKE', '%' . $nickname . '%');
+            $user = db('users')->where($where)->field('id')->find();
+            $user_id = $user['id'];
+            $this->view->assign('user_id', $user_id);
+        }
+
         // 超级管理员
         if ($this->super_power) {
-            $share = db('distribute_orders')->where(['status' => 1])->order('id desc')->paginate(config('list_rows'));
+            if ($user_id) {
+                $share = db('distribute_orders')->where(['status' => 1, 'user_id' => $user_id])->order('id desc')->paginate(config('list_rows'));
+                $data['total'] = db('distribute_orders')->where(['status' => 1, 'user_id' => $user_id])->sum('amount');
+            } else {
+                $share = db('distribute_orders')->where(['status' => 1])->order('id desc')->paginate(config('list_rows'));
+                $data['total'] = db('distribute_orders')->where(['status' => 1])->sum('amount');
+            }
             $shares = $share->toArray();
             foreach ($shares['data'] as $key => $value) {
                 $user_info = db('users')->where(['id' => $value['user_id']])->find();
@@ -45,7 +59,13 @@ class Report extends AdminBase
             $users = db('users')->where(['id' => $this->user['id']])->find();
             if ($users['user_role_id'] == 22) {
                 // 区域管理
-                $user_ids = db('users')->where(['parent_id' => $this->user['id']])->order('id desc')->field('id')->select()->toArray();
+                if ($user_id) {
+                    $user_ids = db('users')->where(['parent_id' => $this->user['id'], 'id' => $user_id])->order('id desc')->field('id')->select()->toArray();
+                    $data['total'] = db('distribute_orders')->where(['status' => 1, 'user_id' => $user_id])->sum('amount');
+                } else {
+                    $user_ids = db('users')->where(['parent_id' => $this->user['id']])->order('id desc')->field('id')->select()->toArray();
+                    $data['total'] = db('distribute_orders')->where(['status' => 1])->sum('amount');
+                }
                 $ids = array_column($user_ids, 'id');
 
                 $where_child['id'] = array('IN', $ids);
@@ -68,7 +88,13 @@ class Report extends AdminBase
                 $this->view->assign('data', $data);
             } elseif ($users['user_role_id'] == 23) {
                 // 县级代理
-                $user_ids = db('users')->where(['parent_id' => $this->user['id']])->order('id desc')->field('id')->select()->toArray();
+                if ($user_id) {
+                    $user_ids = db('users')->where(['parent_id' => $this->user['id'], 'id' => $user_id])->order('id desc')->field('id')->select()->toArray();
+                    $data['total'] = db('distribute_orders')->where(['status' => 1, 'user_id' => $user_id])->sum('amount');
+                } else {
+                    $user_ids = db('users')->where(['parent_id' => $this->user['id']])->order('id desc')->field('id')->select()->toArray();
+                    $data['total'] = db('distribute_orders')->where(['status' => 1])->sum('amount');
+                }
                 $ids = array_column($user_ids, 'id');
                 array_push($ids, $this->user['id']);
 
@@ -96,6 +122,7 @@ class Report extends AdminBase
                     $shares['data'][$key]['user_name'] = $user_info['user_name'];
                 }
 
+                $data['total'] = db('distribute_orders')->where(['status' => 1])->sum('amount');
                 $data['self'] = db('distribute_orders')->where(['status' => 1, 'user_id' => $this->user['id']])->sum('amount');
                 $this->view->assign('data', $data);
             }
@@ -111,8 +138,13 @@ class Report extends AdminBase
     // 分润报表下载
     public function download_profit()
     {
+        $user_id = trim(input('param.user_id', ' ', 'htmlspecialchars'));
         if ($this->super_power) {
-            $share = db()->query('select mhcms_distribute_orders.*,mhcms_users.user_name from mhcms_distribute_orders LEFT JOIN mhcms_users ON mhcms_users.id = mhcms_distribute_orders.user_id ORDER BY id DESC');
+            if ($user_id) {
+                $share = db()->query('select mhcms_distribute_orders.*,mhcms_users.user_name from mhcms_distribute_orders LEFT JOIN mhcms_users ON mhcms_users.id = mhcms_distribute_orders.user_id where mhcms_distribute_orders.user_id = ' . $user_id . ' ORDER BY id DESC');
+            } else {
+                $share = db()->query('select mhcms_distribute_orders.*,mhcms_users.user_name from mhcms_distribute_orders LEFT JOIN mhcms_users ON mhcms_users.id = mhcms_distribute_orders.user_id ORDER BY id DESC');
+            }
         } else {
             // 根据角色查数据
             $users = db('users')->where(['id' => $this->user['id']])->find();
@@ -129,7 +161,12 @@ class Report extends AdminBase
 
                 $ids = implode($ids, ',');
                 $idstr = '(' . $ids . ')';
-                $share = db()->query('select mhcms_distribute_orders.*,mhcms_users.user_name from mhcms_distribute_orders LEFT JOIN mhcms_users ON mhcms_users.id = mhcms_distribute_orders.user_id WHERE mhcms_distribute_orders.status=1 AND mhcms_distribute_orders.user_id IN ' . $idstr . ' ORDER BY id DESC');
+
+                if ($user_id) {
+                    $share = db()->query('select mhcms_distribute_orders.*,mhcms_users.user_name from mhcms_distribute_orders LEFT JOIN mhcms_users ON mhcms_users.id = mhcms_distribute_orders.user_id WHERE mhcms_distribute_orders.status=1 AND mhcms_distribute_orders.user_id = ' . $user_id . ' ORDER BY id DESC');
+                } else {
+                    $share = db()->query('select mhcms_distribute_orders.*,mhcms_users.user_name from mhcms_distribute_orders LEFT JOIN mhcms_users ON mhcms_users.id = mhcms_distribute_orders.user_id WHERE mhcms_distribute_orders.status=1 AND mhcms_distribute_orders.user_id IN ' . $idstr . ' ORDER BY id DESC');
+                }
             } elseif ($users['user_role_id'] == 23) {
                 // 县级代理
                 $user_ids = db('users')->where(['parent_id' => $this->user['id']])->order('id desc')->field('id')->select()->toArray();
@@ -138,7 +175,12 @@ class Report extends AdminBase
 
                 $ids = implode($ids, ',');
                 $idstr = '(' . $ids . ')';
-                $share = db()->query('select mhcms_distribute_orders.*,mhcms_users.user_name from mhcms_distribute_orders LEFT JOIN mhcms_users ON mhcms_users.id = mhcms_distribute_orders.user_id WHERE mhcms_distribute_orders.status=1 AND mhcms_distribute_orders.user_id IN ' . $idstr . ' ORDER BY id DESC');
+
+                if ($user_id) {
+                    $share = db()->query('select mhcms_distribute_orders.*,mhcms_users.user_name from mhcms_distribute_orders LEFT JOIN mhcms_users ON mhcms_users.id = mhcms_distribute_orders.user_id WHERE mhcms_distribute_orders.status=1 AND mhcms_distribute_orders.user_id = ' . $user_id . ' ORDER BY id DESC');
+                } else {
+                    $share = db()->query('select mhcms_distribute_orders.*,mhcms_users.user_name from mhcms_distribute_orders LEFT JOIN mhcms_users ON mhcms_users.id = mhcms_distribute_orders.user_id WHERE mhcms_distribute_orders.status=1 AND mhcms_distribute_orders.user_id IN ' . $idstr . ' ORDER BY id DESC');
+                }
             } else {
                 // 普通用户
                 $share = db()->query('select mhcms_distribute_orders.*,mhcms_users.user_name from mhcms_distribute_orders LEFT JOIN mhcms_users ON mhcms_users.id = mhcms_distribute_orders.user_id WHERE mhcms_distribute_orders.status=1 AND mhcms_distribute_orders.user_id = ' . $this->user['id'] . ' ORDER BY id DESC');
@@ -162,9 +204,29 @@ class Report extends AdminBase
     //充值列表
     public function recharge()
     {
+        $nickname = trim(input('param.nickname', ' ', 'htmlspecialchars'));
+        if ($nickname) {
+            $where['nickname'] = array('LIKE', '%' . $nickname . '%');
+            $user = db('users')->where($where)->field('id')->find();
+            $user_id = $user['id'];
+            $this->view->assign('user_id', $user_id);
+        }
+
+        if ($user_id) {
+            $total = db('orders')->where(['user_id' => $user_id])->sum('total_fee');
+            $this->view->assign('total', $total);
+        } else {
+            $total = db('orders')->sum('total_fee');
+            $this->view->assign('total', $total);
+        }
+
         // 超级管理员
         if ($this->super_power) {
-            $recharge = db('orders')->order('id desc')->paginate(config('list_rows'));
+            if ($user_id) {
+                $recharge = db('orders')->where(['user_id' => $user_id])->order('id desc')->paginate(config('list_rows'));
+            } else {
+                $recharge = db('orders')->order('id desc')->paginate(config('list_rows'));
+            }
             $recharges = $recharge->toArray();
             foreach ($recharges['data'] as $key => $value) {
                 $user_info = db('users')->where(['id' => $value['user_id']])->find();
@@ -175,7 +237,11 @@ class Report extends AdminBase
             $users = db('users')->where(['id' => $this->user['id']])->find();
             if ($users['user_role_id'] == 22) {
                 // 区域管理
-                $user_ids = db('users')->where(['parent_id' => $this->user['id']])->order('id desc')->field('id')->select()->toArray();
+                if ($user_id) {
+                    $user_ids = db('users')->where(['parent_id' => $this->user['id'], 'id' => $user_id])->order('id desc')->field('id')->select()->toArray();
+                } else {
+                    $user_ids = db('users')->where(['parent_id' => $this->user['id']])->order('id desc')->field('id')->select()->toArray();
+                }
                 $ids = array_column($user_ids, 'id');
 
                 $where_child['id'] = array('IN', $ids);
@@ -194,7 +260,11 @@ class Report extends AdminBase
                 }
             } elseif ($users['user_role_id'] == 23) {
                 // 县级代理
-                $user_ids = db('users')->where(['parent_id' => $this->user['id']])->order('id desc')->field('id')->select()->toArray();
+                if ($user_id) {
+                    $user_ids = db('users')->where(['parent_id' => $this->user['id'], 'id' => $user_id])->order('id desc')->field('id')->select()->toArray();
+                } else {
+                    $user_ids = db('users')->where(['parent_id' => $this->user['id']])->order('id desc')->field('id')->select()->toArray();
+                }
                 $ids = array_column($user_ids, 'id');
                 array_push($ids, $this->user['id']);
 
@@ -229,8 +299,14 @@ class Report extends AdminBase
     //充值列表下载
     public function download_recharge()
     {
+        $user_id = trim(input('param.user_id', ' ', 'htmlspecialchars'));
+
         if ($this->super_power) {
-            $recharge = db()->query('select mhcms_orders.*,mhcms_users.user_name from mhcms_orders LEFT JOIN mhcms_users ON mhcms_users.id = mhcms_orders.user_id ORDER BY id DESC');
+            if ($user_id) {
+                $recharge = db()->query('select mhcms_orders.*,mhcms_users.user_name from mhcms_orders LEFT JOIN mhcms_users ON mhcms_users.id = mhcms_orders.user_id where mhcms_orders.user_id=' . $user_id . ' ORDER BY id DESC');
+            } else {
+                $recharge = db()->query('select mhcms_orders.*,mhcms_users.user_name from mhcms_orders LEFT JOIN mhcms_users ON mhcms_users.id = mhcms_orders.user_id ORDER BY id DESC');
+            }
         } else {
             // 根据角色查数据
             $users = db('users')->where(['id' => $this->user['id']])->find();
@@ -247,7 +323,12 @@ class Report extends AdminBase
 
                 $ids = implode($ids, ',');
                 $idstr = '(' . $ids . ')';
-                $recharge = db()->query('select mhcms_orders.*,mhcms_users.user_name from mhcms_orders LEFT JOIN mhcms_users ON mhcms_users.id = mhcms_orders.user_id WHERE mhcms_distribute_orders.user_id IN ' . $idstr . ' ORDER BY id DESC');
+
+                if ($user_id) {
+                    $recharge = db()->query('select mhcms_orders.*,mhcms_users.user_name from mhcms_orders LEFT JOIN mhcms_users ON mhcms_users.id = mhcms_orders.user_id WHERE mhcms_distribute_orders.user_id = ' . $user_id . ' ORDER BY id DESC');
+                } else {
+                    $recharge = db()->query('select mhcms_orders.*,mhcms_users.user_name from mhcms_orders LEFT JOIN mhcms_users ON mhcms_users.id = mhcms_orders.user_id WHERE mhcms_distribute_orders.user_id IN ' . $idstr . ' ORDER BY id DESC');
+                }
             } elseif ($users['user_role_id'] == 23) {
                 // 县级代理
                 $user_ids = db('users')->where(['parent_id' => $this->user['id']])->order('id desc')->field('id')->select()->toArray();
@@ -256,7 +337,11 @@ class Report extends AdminBase
 
                 $ids = implode($ids, ',');
                 $idstr = '(' . $ids . ')';
-                $recharge = db()->query('select mhcms_orders.*,mhcms_users.user_name from mhcms_orders LEFT JOIN mhcms_users ON mhcms_users.id = mhcms_orders.user_id WHERE mhcms_distribute_orders.user_id IN ' . $idstr . ' ORDER BY id DESC');
+                if ($user_id) {
+                    $recharge = db()->query('select mhcms_orders.*,mhcms_users.user_name from mhcms_orders LEFT JOIN mhcms_users ON mhcms_users.id = mhcms_orders.user_id WHERE mhcms_distribute_orders.user_id = ' . $user_id . ' ORDER BY id DESC');
+                } else {
+                    $recharge = db()->query('select mhcms_orders.*,mhcms_users.user_name from mhcms_orders LEFT JOIN mhcms_users ON mhcms_users.id = mhcms_orders.user_id WHERE mhcms_distribute_orders.user_id IN ' . $idstr . ' ORDER BY id DESC');
+                }
             } else {
                 // 普通用户
                 $recharge = db()->query('select mhcms_orders.*,mhcms_users.user_name from mhcms_orders LEFT JOIN mhcms_users ON mhcms_users.id = mhcms_orders.user_id WHERE mhcms_distribute_orders.user_id = ' . $this->user['id'] . ' ORDER BY id DESC');
