f52ee4266dcf30e0e9aff82a8de8ce4e503a347e
yanzhiwei <yanzhiwei@360.net>
2018-10-19 17:32:40 +0800

    神奇的首页消失了

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -27,6 +27,9 @@ class Index extends AdminBase
 
     public function main()
     {
+        global $_W;
+
+        return '<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"><span style="font-size:30px">'.$_W['site']['config']['system_name'].'</span></p></div></script>';
         $where = [];
         //$where['is_app'] = 1;
         //$where['is_o2o'] = 1;
